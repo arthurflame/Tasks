f17c186f550e40ae816a72fa740c7b4f6786aab5
Arthurflame <artgerash@gmail.com>
2021-09-05 13:31:32 +0300

    upd

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -xe
+
+# install packages and dependencies
+go get github.com/thewhitetulip/Tasks/config
+go get github.com/thewhitetulip/Tasks/views
+# build command
+go build -o bin/application application.go
