c6af52ba6b86d877880989b84a79808e97ae3cba
Arthurflame <artgerash@gmail.com>
2021-09-06 11:08:06 +0300

    upd

--- build.sh
+++ build.sh
@@ -2,7 +2,9 @@
 set -xe
 
 # install packages and dependencies
+go mod init Tasks
+unset GOPATH
 go get github.com/thewhitetulip/Tasks/config
 go get github.com/thewhitetulip/Tasks/views
 # build command
-go build -o bin/application application.go
+#go build -o bin/application application.go
