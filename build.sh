bf0b9a699368ea59bfe78436d21e29c906f841dd
test name <test@test.com>
2021-09-07 10:28:52 +0000

    upd

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ go mod init Tasks
 unset GOPATH
 go get github.com/thewhitetulip/Tasks/config
 go get github.com/thewhitetulip/Tasks/views
-# build command
-#go build -o bin/application application.go
+#build command
+go build -o bin/application application.go
